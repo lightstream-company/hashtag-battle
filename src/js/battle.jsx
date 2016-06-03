931f60b63d617a23c48d8112c4dd228013449b87
BenoitArguel <benoit.arguel@gmail.com>
2016-06-03 17:26:55 +0200

    task: adding new style for battle widget

--- src/js/battle.jsx
+++ src/js/battle.jsx
@@ -1,43 +1,64 @@
 import React, {PropTypes} from 'react';
 
 function Battle(){
-  const styleContainer = {
-    width: 1100
-  };
-  const styleImg = {
-    width: 100,
-    height: 100,
-    borderRadius: 3
+
+  var styleContainer = {
+    width: 1100,
+    margin: 'auto'
   };
 
-  const color1 = 'red';
+  const hashtag1 = '#Trololo';
+  const hashtag2 = '#Trululu';
+  const color1 = 'white';
   const color2 = 'green';
-  const count1 = 456;
+  const count1 = 256;
   const count2 = 678;
+  
   const count1Percent = 100 * count1 / ( count1 + count2 );
   const count2Percent = 100 * count2 / ( count1 + count2 );
-  const widthPlots = styleContainer.width - ( styleImg.width * 2 ) - 45;
-  console.log(widthPlots); 
+  const widthPlots = styleContainer.width - 200 - 45;
 
-  const stylePlot1 = {
-    background: color1,
-    marginLeft: styleImg.width + 15,
+  const styleFighter1 = {
+    marginLeft: 15,
     width: ( widthPlots / 100 ) * count1Percent
   };
-  const stylePlot2 = {
-    background: color2,
-    marginRight: styleImg.width + 15,
+  const styleFighter2 = {
+    marginRight: 15,
     width: ( widthPlots / 100 ) * count2Percent
+  };
+  const stylePlot1 = {
+    background: color1
+  };
+  const stylePlot2 = {
+    background: color2
+  };
+  const styleSpan1 = {
+    float: 'right',
+    transform: 'translateX(-15px) translateY(9px)',
+    color: color2
+  };
+  const styleSpan2 = {
+    float: 'left',
+    transform: 'translateX(15px) translateY(9px)',
+    color: color1
   };
 
   return <div className="container-battle" style={styleContainer}>
-    <div className="fighter1">
-      <img style={styleImg} className="img1" src="http://www.lexilogos.com/images/gb_drapeau.gif" />
-      <div style={stylePlot1} className="plots"></div>
+    <img className="img1" src="http://www.lexilogos.com/images/gb_drapeau.gif" />
+    <div style={styleFighter1} className="fighter1">
+      <p className="hashtag">{hashtag1}</p>
+      <p className="count">{count1} tweets</p>
+      <div className="plot1" style={stylePlot1}>
+        <span style={styleSpan1} >{Math.round(count1Percent)}%</span>
+      </div>
     </div>
-    <div className="fighter2">
-      <img style={styleImg} className="img2" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Flag_of_Germany.svg/langfr-225px-Flag_of_Germany.svg.png" />
-      <div style={stylePlot2} className="plots"></div>
+    <img className="img2" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Flag_of_Germany.svg/langfr-225px-Flag_of_Germany.svg.png" />
+    <div style={styleFighter2} className="fighter2">
+      <p className="hashtag p-right">{hashtag2}</p>
+      <p className="count p-right">{count2} tweets</p>
+      <div className="plot2" style={stylePlot2}>
+        <span style={styleSpan2} >{Math.round(count2Percent)}%</span>
+      </div>
     </div>
   </div>;
 }
