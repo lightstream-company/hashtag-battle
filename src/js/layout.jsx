ccb3c98ad4df981311608199bd243e253b936105
BenoitArguel <benoit.arguel@gmail.com>
2016-06-03 18:04:20 +0200

    task: initialization tweet-wall widget

--- src/js/layout.jsx
+++ src/js/layout.jsx
@@ -1,11 +1,13 @@
 import Battle from './battle.jsx';
 import Header from './header.jsx';
+import Wall from './wall.jsx';
 import React from 'react';
 
 function Layout() {
   return <div>
     <Header />
     <Battle />
+    <Wall />
   </div>;
 }
 
