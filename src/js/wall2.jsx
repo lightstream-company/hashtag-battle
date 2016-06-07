7f121e613235e5e03d09d450e809606de79cdbd7
BenoitArguel <benoit.arguel@gmail.com>
2016-06-07 09:39:48 +0200

    task : adding new background style

--- src/js/wall2.jsx
+++ src/js/wall2.jsx
@@ -5,12 +5,86 @@ function Wall2(){
     maxWidth: 1100,
     margin: 'auto'
   };
-  var styleContainerLeft = {
+  var styleContainerRight = {
     width: 49 + '%',
     float: 'right'
   };
   return <div style={styleContainer} className="container-wall">
-    <div style={styleContainerLeft} className="container-wall-left">
+    <div style={styleContainerRight} className="container-wall-right">
+
+      
+      <div className="tweet">
+        <img src="http://s3.amazonaws.com/37assets/svn/765-default-avatar.png" />
+        <div className="tweet-text">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad mini.</p>
+        </div>
+      </div>
+
+      <div className="tweet">
+        <img src="http://s3.amazonaws.com/37assets/svn/765-default-avatar.png" />
+        <div className="tweet-text">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad mini.</p>
+        </div>
+      </div>
+
+      <div className="tweet">
+        <img src="http://s3.amazonaws.com/37assets/svn/765-default-avatar.png" />
+        <div className="tweet-text">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad mini.</p>
+        </div>
+      </div>
+      
+      <div className="tweet">
+        <img src="http://s3.amazonaws.com/37assets/svn/765-default-avatar.png" />
+        <div className="tweet-text">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad mini.</p>
+        </div>
+      </div>
+
+      <div className="tweet">
+        <img src="http://s3.amazonaws.com/37assets/svn/765-default-avatar.png" />
+        <div className="tweet-text">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad mini.</p>
+        </div>
+      </div>
+
+      <div className="tweet">
+        <img src="http://s3.amazonaws.com/37assets/svn/765-default-avatar.png" />
+        <div className="tweet-text">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad mini.</p>
+        </div>
+      </div>
+    
+      
+      <div className="tweet">
+        <img src="http://s3.amazonaws.com/37assets/svn/765-default-avatar.png" />
+        <div className="tweet-text">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad mini.</p>
+        </div>
+      </div>
+
+      <div className="tweet">
+        <img src="http://s3.amazonaws.com/37assets/svn/765-default-avatar.png" />
+        <div className="tweet-text">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad mini.</p>
+        </div>
+      </div>
+
+      <div className="tweet">
+        <img src="http://s3.amazonaws.com/37assets/svn/765-default-avatar.png" />
+        <div className="tweet-text">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad mini.</p>
+        </div>
+      </div>
+      
+      <div className="tweet">
+        <img src="http://s3.amazonaws.com/37assets/svn/765-default-avatar.png" />
+        <div className="tweet-text">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad mini.</p>
+        </div>
+      </div>
+
+
     </div>
   </div>;
 }
