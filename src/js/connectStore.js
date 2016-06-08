8984986928effe2c5a8f093eff077579891fc6f8
Franck Ernewein <franck.ernewein@gmail.com>
2016-06-08 23:31:01 +0200

    task(react): connect wall to store

--- src/js/connectStore.js
+++ src/js/connectStore.js
@@ -1,28 +1,20 @@
 import createConnection from 'tweetping-connect';
 import { limit } from './config';
 
-export default function connectStore(store, id){
+export default function connectStore(store, id) {
 
-    function dispatchPost(post) {
-      store.dispatch({
-        type: id,
-        post
-      });
-    }
-    const {connect, load} = createConnection(id);
-    connect('wall', dispatchPost);
-
-    load('wall/', {
-      query: {
-        size: limit
-      }
-    }).then(posts => {
-      function loop(n){
-        dispatchPost(posts[n]);
-        setTimeout(()=>{
-          loop(n<limit-1 ? n+1:0);
-        }, 30 / 10);
-      }
-      loop(0);
+  function dispatchPost(post) {
+    store.dispatch({
+      type: id,
+      post
     });
+  }
+  const {connect, load} = createConnection(id);
+  connect('wall', dispatchPost);
+
+  load('wall/', {
+    query: {
+      size: limit
+    }
+  }).then(posts => posts.forEach(dispatchPost));
 }
--- src/js/store.js
+++ src/js/store.js
@@ -1,12 +1,14 @@
 import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
-import createWallReducer from './createWallReducer';
+import {createWallReducer, createCountReducer} from './createReducer';
 import connectStore from './connectStore';
 import thunk from 'redux-thunk';
 import { stream1, stream2 } from './config';
 
 const store = createStore(combineReducers({
   wall1: createWallReducer(stream1),
-  wall2: createWallReducer(stream2)
+  wall2: createWallReducer(stream2),
+  count1: createCountReducer(stream1),
+  count2: createCountReducer(stream2)
 }), [], compose(
   applyMiddleware(thunk),
   window.devToolsExtension ? window.devToolsExtension() : f => f
