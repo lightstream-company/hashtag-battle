9b8ef99b8867cff8065a77ccc132b3dd701b8805
Franck Ernewein <franck.ernewein@gmail.com>
2016-06-09 19:44:56 +0200

    feat(counter): fetch history from server

--- src/js/createReducer.js
+++ src/js/createReducer.js
@@ -1,24 +1,26 @@
 import { limit } from './config';
 
-export function createWallReducer(id){
+export function createWallReducer(id) {
   return function wall(state = [], action) {
     const {post, type} = action;
-    switch(type){
+    switch (type) {
       case id:
         return [...state, post].slice(-limit);
-      default: 
+      default:
         return state;
     }
   };
 }
 
-export function createCountReducer(id){
+export function createCountReducer(id) {
   return function count(state = 0, action) {
-    const {type} = action;
-    switch(type){
+    const {type, value} = action;
+    switch (type) {
+      case 'INIT_COUNT_' + id:
+        return state + value;
       case id:
         return state + 1;
-      default: 
+      default:
         return state;
     }
   };
