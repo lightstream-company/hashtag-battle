19f11747f96a8b75f105f75c26f39bf4aaf4dfbe
Franck Ernewein <franck.ernewein@gmail.com>
2016-06-10 18:59:26 +0200

    task(hash): reactivate hash stream selection

--- src/js/config.js
+++ src/js/config.js
@@ -1,14 +1,9 @@
-/*
 import parseHash from './utils/parseHash';
 
 const teams = parseHash(document.location.hash);
 
 export const stream1 = teams[0];
 export const stream2 = teams[1];
-*/
-
-export const stream1 = 'overwatch';
-export const stream2 = 'uefa';
 
 export const limit = 10;
 export const tweetHeight = 48;
