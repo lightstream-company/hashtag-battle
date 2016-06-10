04449d54ae22b9a2d5c59a472359350f38a49ba7
BenoitArguel <benoit.arguel@gmail.com>
2016-06-10 17:41:16 +0200

    task: new style for header

--- src/js/header.jsx
+++ src/js/header.jsx
@@ -8,7 +8,10 @@ function Header(){
   };
   return <div className="header" style={styleHeader}>
     <div className="container-tweetping-github">
-      <a className="github" href="https://github.com/lightstream-company/hashtag-battle" target="blank"><i className="fa fa-github" aria-hidden="true"></i></a>
+      <div className="github" >
+        <a href="https://github.com/lightstream-company/hashtag-battle" target="blank"><i className="fa fa-github" aria-hidden="true"></i></a>
+        <p>View the code <br />on github</p>
+      </div>
       <div className="tweetping">
         <p>Powered by</p>
         <p><a href="https://www.tweetping.net/" target="blank">Tweetping</a></p>
